f3c74e35e5212acbca7cb3042c3dc34dff9e7d5d
HCH1 <theone_730801@hotmail.com>
2016-10-11 22:46:39 +0800

    Update and rename SOPrun_wc.py to main1_wc.py

--- main1_wc.py
+++ main1_wc.py
@@ -1,11 +1,11 @@
-file=open("28LPSErulesALL_orig","r+")
+file=open(".txt","r+")
 wordcount={}
 for word in file.read().split():
     if word not in wordcount:
         wordcount[word] = 1
     else:
         wordcount[word] += 1
-#log = open("wc_28LPSE.txt", "w");
+#log = open(".txt", "w");
 for k,v in wordcount.items():
     #print >>log, k, v
     print k, v
