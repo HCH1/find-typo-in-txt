74d9ac197060808c882d672baf409ca0546b8415
HCH1 <theone_730801@hotmail.com>
2016-11-15 22:46:54 +0800

    Update demo1115WCv1.R

--- demo1115WCv1.R
+++ demo1115WCv1.R
@@ -1,3 +1,4 @@
+## http://www.sthda.com/english/wiki/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know#reasons-you-should-use-word-clouds-to-present-your-text-data
 # Install
 # install.packages("tm")  # for text mining
 # install.packages("SnowballC") # for text stemming
@@ -14,15 +15,23 @@ text <- readLines("/Users/HCHung/Documents/2016demo/demo1115WCbyR/28LPSErulesALL
 docs <- Corpus(VectorSource(text))
 # inspect(docs)
 toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
-docs <- tm_map(docs, toSpace, "/")
-docs <- tm_map(docs, toSpace, "@")
-docs <- tm_map(docs, toSpace, "\\|")
+docs <- tm_map(docs, toSpace, "\\(")
+docs <- tm_map(docs, toSpace, "\\)")
+docs <- tm_map(docs, toSpace, "\\[")
+docs <- tm_map(docs, toSpace, "\\]")
+docs <- tm_map(docs, toSpace, "\\{")
+docs <- tm_map(docs, toSpace, "\\}")
+# docs <- tm_map(docs, toSpace, "\\.")
+docs <- tm_map(docs, toSpace, "\\,")
+# docs <- tm_map(docs, toSpace, "/")
+# docs <- tm_map(docs, toSpace, "@")
+# docs <- tm_map(docs, toSpace, "\\|")
 # Convert the text to lower case
 docs <- tm_map(docs, content_transformer(tolower))
 # Remove numbers
 # docs <- tm_map(docs, removeNumbers)
 # Remove punctuations
-docs <- tm_map(docs, removePunctuation)
+# docs <- tm_map(docs, removePunctuation)
 # Eliminate extra white spaces
 docs <- tm_map(docs, stripWhitespace)
 # Text stemming
@@ -32,3 +41,12 @@ m <- as.matrix(dtm)
 v <- sort(rowSums(m),decreasing=TRUE)
 d <- data.frame(word = names(v),freq=v)
 head(d, 10)
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+##
+barplot(d[1:20,]$freq, las = 2, names.arg = d[1:20,]$word,
+        col ="lightblue", main ="Freq words",
+        ylab = "freq")
+##
