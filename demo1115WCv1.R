2331fd6cd8cec29b7af5c7a162ee348d51aa848a
HCH1 <theone_730801@hotmail.com>
2016-11-15 21:30:09 +0800

    Add files via upload

---
+++ demo1115WCv1.R
@@ -0,0 +1,34 @@
+# Install
+# install.packages("tm")  # for text mining
+# install.packages("SnowballC") # for text stemming
+# install.packages("wordcloud") # word-cloud generator 
+# install.packages("RColorBrewer") # color palettes
+# Load
+# library("tm")
+# library("SnowballC")
+# library("wordcloud")
+# library("RColorBrewer")
+# Read the text file
+text <- readLines("/Users/HCHung/Documents/2016demo/demo1115WCbyR/28LPSErulesALL_orig.txt")
+# Load the data as a corpus
+docs <- Corpus(VectorSource(text))
+# inspect(docs)
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+# docs <- tm_map(docs, removeNumbers)
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
