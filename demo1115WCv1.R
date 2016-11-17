4b06f0f3c90bc5fcfcef8eb3af1eb1446c820be0
HCH1 <theone_730801@hotmail.com>
2016-11-17 20:06:25 +0800

    Update demo1115WCv1.R

--- demo1115WCv1.R
+++ demo1115WCv1.R
@@ -1,49 +1,32 @@
 ## http://www.sthda.com/english/wiki/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know#reasons-you-should-use-word-clouds-to-present-your-text-data
-# Install
 # install.packages("tm")  # for text mining
 # install.packages("SnowballC") # for text stemming
 # install.packages("wordcloud") # word-cloud generator 
 # install.packages("RColorBrewer") # color palettes
-# Load
+# install.packages("hunspell")
+# library("hunspell")
 # library("tm")
 # library("SnowballC")
 # library("wordcloud")
 # library("RColorBrewer")
-# Read the text file
 text <- readLines("/Users/HCHung/Documents/2016demo/demo1115WCbyR/28LPSErulesALL_orig.txt")
-# Load the data as a corpus
 docs <- Corpus(VectorSource(text))
-# inspect(docs)
-toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
-docs <- tm_map(docs, toSpace, "\\(")
-docs <- tm_map(docs, toSpace, "\\)")
-docs <- tm_map(docs, toSpace, "\\[")
-docs <- tm_map(docs, toSpace, "\\]")
-docs <- tm_map(docs, toSpace, "\\{")
-docs <- tm_map(docs, toSpace, "\\}")
-# docs <- tm_map(docs, toSpace, "\\.")
-docs <- tm_map(docs, toSpace, "\\,")
-# docs <- tm_map(docs, toSpace, "/")
-# docs <- tm_map(docs, toSpace, "@")
-# docs <- tm_map(docs, toSpace, "\\|")
-# Convert the text to lower case
+# docs <- gsub("rx","rrxx",docs)
+docs <- gsub("[[^A-Za-z0-9]]"," ",docs)
+docs <- gsub("[[:punct:]]"," ",docs)
+# toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+# docs <- tm_map(docs, toSpace, "\\(")
 docs <- tm_map(docs, content_transformer(tolower))
-# Remove numbers
-# docs <- tm_map(docs, removeNumbers)
-# Remove punctuations
-# docs <- tm_map(docs, removePunctuation)
-# Eliminate extra white spaces
 docs <- tm_map(docs, stripWhitespace)
-# Text stemming
-# docs <- tm_map(docs, stemDocument)
 dtm <- TermDocumentMatrix(docs)
 m <- as.matrix(dtm)
 v <- sort(rowSums(m),decreasing=TRUE)
 d <- data.frame(word = names(v),freq=v)
-write.table(x = d, file = "o1.csv", sep = ",", 
+write.table(x = d, file = "o11.csv", sep = ",", 
             col.names = NA, qmethod = "double")
 head(d, 10)
 set.seed(1234)
+##
 wordcloud(words = d$word, freq = d$freq, min.freq = 1,
           max.words=200, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
@@ -52,12 +35,11 @@ barplot(d[1:20,]$freq, las = 2, names.arg = d[1:20,]$word,
         col ="lightblue", main ="Freq words",
         ylab = "freq")
 ##
-library(hunspell)
 # words <- c("beer", "wiskey", "wine")
-words <- readLines("/Users/HCHung/Documents/2016demo/demo1115WCbyR/o1.csv")
+words <- readLines("/Users/HCHung/Documents/2016demo/demo1115WCbyR/o11.csv")
 # hunspell_check(words)
 bad_words <- hunspell(words)
 head(bad_words, 10)
 # write(bad_words, file = "o2.txt", sep = "\t")
-lapply(bad_words, write, "o3.txt", append=TRUE, ncolumns=1000)
+lapply(bad_words, write, "o33.txt", append=TRUE, ncolumns=1000)
 ##
